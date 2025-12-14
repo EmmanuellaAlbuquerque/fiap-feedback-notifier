7880ec70188642f984b34536889d829385d567be
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2025-12-14 17:24:33 -0300

    feat: definindo estrutura base do envio de emails com ses

---
+++ src/main/java/notifier/SesClientProducer.java
@@ -0,0 +1,21 @@
+package notifier;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@ApplicationScoped
+public class SesClientProducer {
+
+    @Produces
+    public SesClient sesClient() {
+        return SesClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.US_EAST_1)
+                .httpClient(UrlConnectionHttpClient.builder().build())
+                .build();
+    }
+}
