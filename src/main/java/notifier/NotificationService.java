a3179e0097ef9c0fbb25fe149a207d1557108303
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2025-12-14 15:37:36 -0300

    feat: definindo estrutura base do envio de emails

--- src/main/java/notifier/NotificationService.java
+++ src/main/java/notifier/NotificationService.java
@@ -2,7 +2,6 @@ package notifier;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
@@ -13,28 +12,21 @@ public class NotificationService {
     @Inject
     SnsClient snsClient;
 
+    @Inject
+    EmailTemplateService emailTemplateService;
+
     @ConfigProperty(name = "sns.urgency.topic.arn")
     String urgencyTopicArn;
 
     public void notifyUrgency(FeedbackMessage feedback) {
-
-        String message = """
-            Feedback Crítico Recebido
-
-            Nota: %d
-            Descrição: %s
-            Data: %s
-            
-            """.formatted(
-                feedback.nota(),
-                feedback.descricao(),
-                feedback.dataCriacao()
-        );
+        String htmlMessage = emailTemplateService.buildHtmlMessage(feedback);
+        UrgencyLevel urgency = UrgencyLevel.fromScore(feedback.nota());
 
         snsClient.publish(
                 PublishRequest.builder()
                         .topicArn(urgencyTopicArn)
-                        .message(message)
+                        .subject("Feedback Crítico - Urgência: " + urgency.getLabel())
+                        .message(htmlMessage)
                         .build()
         );
     }
